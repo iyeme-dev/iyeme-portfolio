a835c08180834ec2dee46f8ad428bc315ae2228b
Iyeme Salubi <iyemeaji@yahoo.com>
2026-09-20 11:02:49 +0100

    Update floating navigation for data engineering sections

--- src/sections/floating-nav/data.js
+++ src/sections/floating-nav/data.js
@@ -1,17 +1,14 @@
-import {AiFillHome} from 'react-icons/ai'
+import {AiFillHome, AiFillAppstore, AiFillMessage} from 'react-icons/ai'
 import {BsPersonFill} from 'react-icons/bs'
-import {MdHomeRepairService} from 'react-icons/md'
-import {AiFillAppstore} from 'react-icons/ai'
-import {AiFillMessage} from 'react-icons/ai'
-
+import {FaDatabase, FaBriefcase} from 'react-icons/fa'
 
 const data = [
     {id: 1, link: '#', icon: <AiFillHome/>},
     {id: 2, link: '#about', icon: <BsPersonFill/>},
-    {id: 3, link: '#services', icon: <MdHomeRepairService/>},
-    {id: 4, link: '#portfolio', icon: <AiFillAppstore/>},
-    {id: 5, link: '#contact', icon: <AiFillMessage/>}
+    {id: 3, link: '#skills', icon: <FaDatabase/>},
+    {id: 4, link: '#experience', icon: <FaBriefcase/>},
+    {id: 5, link: '#portfolio', icon: <AiFillAppstore/>},
+    {id: 6, link: '#contact', icon: <AiFillMessage/>}
 ]
 
-
-export default data
+export default data;
